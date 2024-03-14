a258a94d31301bf1205ab82f473ae6d75e0c22d0
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-14 15:41:31 +0700

    add relative path init

--- app/Config/Filepicker.php
+++ app/Config/Filepicker.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
+
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
+
 class Filepicker extends BaseConfig
 {
 	// Relative path
+	public $uploadURL;
+	public $iconURL;
 	public $uploadPath = 'public/files/uploads/';
 	public $serverURL = 'filepicker/';
 	public $iconPath = 'public/images/filepicker_images/';
 	public $itemPerPage = 50;
 	public $thumbnail = [
-						'small' => ['w' => 250, 'h' => 250],
-						'medium' => ['w' => 450, 'h' => 450]
-					
-					];
+		'small' => ['w' => 250, 'h' => 250],
+		'medium' => ['w' => 450, 'h' => 450]
 
-	public function __construct() 
+	];
+
+	public function __construct()
 	{
 		$config = new \Config\App();
 		$this->uploadURL = $config->baseURL . $this->uploadPath;
