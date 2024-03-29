0348b7dbae15832ab322eef5d3e58c174a51a9a2
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-29 14:51:30 +0700

    update pull

--- pull.php
+++ pull.php
@@ -2,6 +2,10 @@
 $output = shell_exec('git pull origin master');
 $view_landing = shell_exec('cd app/Views/themes/modern/landing-page/ && git pull');
 $public_landing = shell_exec('cd public/landing-page/ && git pull');
-echo "<p>SymptoMed-Website: $output<br>view_landing: $view_landing<br>public_landing: $public_landing
-    </p>";
+echo "  <b>Status GIT PULL</b>
+<pre>
+    SymptoMed-Website: $output
+    view_landing: $view_landing
+    public_landing: $public_landing
+</pre>";
 ?>
