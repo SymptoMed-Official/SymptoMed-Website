2fbbd1e289831e1c43a2053a3cf57be1df88c099
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-30 22:50:46 +0700

    edit api home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,18 +1,27 @@
-<?php 
+<?php
+
 namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function __construct() 
+	protected $urlAPI, $urlAdminAPI, $urlDoctorAPI;
+	public function __construct()
 	{
 		parent::__construct();
+		$this->urlAPI = 'https://symptomed-401211.et.r.appspot.com';
+		$this->urlAdminAPI = 'https://symptomed-admin-api.herokuapp.com';
+		$this->urlDoctorAPI = 'https://symptomed-doctor-api.herokuapp.com';
 	}
 	public function index()
 	{
-		$this->data ['site_title'] = 'SymptoMed, your personal healthcare solution!';
-		return view('themes/modern/landing-page/public/index', $this->data);
+		$dataCallAPI = [
+			'method' => 'GET',
+			'path' => $this->urlAPI . '/article?type=all',
+		];
+		$response = $this->callApiPublic($dataCallAPI);
+		$this->data['api'] = json_decode($response->getBody(), true);
+		$this->data['site_title'] = 'SymptoMed, your personal healthcare solution!';
+		$this->data['site_desc'] = 'SymptoMed merupakan sebuah aplikasi mobile yang bertujuan untuk mendukung pelayanan kesehatan masyarakat Indonesia secara merata, mudah, mandiri serta akurat menggunakan pendekatan kecerdasan buatan (Artificial Intelligence/AI).';
+		return view('themes/modern/home/index', $this->data);
 	}
-
-	//--------------------------------------------------------------------
-
-}
+};
