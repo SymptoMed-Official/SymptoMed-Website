3b01d231aac2951a3fabe5ed0ab21c370165320d
rama wandya <ramawandia153@gmail.com>
2024-04-07 21:54:48 +0700

    feat: add cover view

--- app/Views/themes/modern/cover.php
+++ app/Views/themes/modern/cover.php
@@ -1,42 +1,24 @@
-<?php
-
-$text = $api;
-$image = $text['url'];
-$image = base64_encode(file_get_contents($image));
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <style>
-        .button{
-            background-color: #0D6EFD;
-            border: none;
-            color: white;
-            padding: 10px 12px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 10%;
-        }
-    </style>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cover</title>
-</head>
-<body>
-    <p><?php echo "version: " . $text['version'] ?></p>
-    <?php echo '<img src="data:image/png;base64,'.$image.'" width="auto" height="200">';?><br>
-    <br>
-    <br>
-    <br>
-    <form action="Cover.php">
-        <input type="file" id="cover" name="cover"  accept="image/png, image/gif, image/jpeg">
-        <input type="submit" class="button">
+<div class="card">
+  <div class="card-header">
+    <h5 class="card-title"><?= $current_module['judul_module'] ?></h5>
+  </div>
+  <div class="card-body">
+    <?php
+    // Default untuk alert
+    if (!empty($message)) {
+      show_alert($message);
+    } ?>
+    <!-- Kode untuk ada button diatas garis/ tag HR -->
+    <form action="cover" method="post">
+      <input type="file" id="cover" name="cover" accept="image/png, image/jpeg">
+      <input type="submit" class="btn btn-primary">
     </form>
     
-</body>
-</html>
+    <hr />
+    <!-- Tambahkan kode Disini -->
+    
+    <script>
+      // Script disini
+    </script>
+  </div>
+</div>
