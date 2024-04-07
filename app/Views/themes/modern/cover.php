fb7ba4ece88a0c99291e0d869ee05dd8555641d6
rama wandya <ramawandia153@gmail.com>
2024-04-07 22:12:06 +0700

    fix: add preview and version

--- app/Views/themes/modern/cover.php
+++ app/Views/themes/modern/cover.php
@@ -1,3 +1,10 @@
+<?php
+
+    $text = $api;
+    $image = $text['url'];
+    $image = base64_encode(file_get_contents($image));
+    ?>
+
 <div class="card">
   <div class="card-header">
     <h5 class="card-title"><?= $current_module['judul_module'] ?></h5>
@@ -8,6 +15,11 @@
     if (!empty($message)) {
       show_alert($message);
     } ?>
+
+    <p><?php echo "version: " . $text['version'] ?></p>
+    <?php echo '<img src="data:image/png;base64,'.$image.'" width="auto" height="200">';?><br>
+
+    
     <!-- Kode untuk ada button diatas garis/ tag HR -->
     <form action="cover" method="post">
       <input type="file" id="cover" name="cover" accept="image/png, image/jpeg">
